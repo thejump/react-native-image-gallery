44f46cbc5ba95c11d928025eabd480eb48814d07
jcharbo <jcharbo@users.noreply.github.com>
2018-06-26 16:24:47 -0500

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -229,7 +229,7 @@ export default class Gallery extends PureComponent {
         return (
             <TransformableImage
               onViewTransformed={((transform) => {
-            global.warn('a1',transform)
+            global.warn('a1',{transform,a:transform.scale==1,b:transform.scale!=1,c:transform.scale=='1'})
             if(transform && transform.scale!=1){
                 global.warn('a2')
                 if(!this.transformed){
